06dda34c6ddde58ce697988fcceaf59a63df6890
Andika Elang D <andika.elang2511@gmail.com>
2024-12-26 09:51:57 +0700

    refactor(role permission): menambah izin master operator

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -15,7 +15,17 @@ class RolePermissionSeeder extends Seeder
     public function run(): void
     {
          // Membuat Permissions
-         $permissions = ['create-master-status-operasi', 'edit-master-status-operasi', 'delete-master-status-operasi', 'edit-urutan-master-status-operasi', 'edit-master-ok', 'update-status-ok'];
+         $permissions = [
+            'create-master-status-operasi',
+            'edit-master-status-operasi',
+            'delete-master-status-operasi',
+            'edit-urutan-master-status-operasi',
+            'edit-master-ok',
+            'update-status-ok',
+            'create-master-operator',
+            'edit-master-operator',
+            'delete-master-operator'
+         ];
 
          foreach ($permissions as $permission) {
              Permission::create(['name' => $permission]);
